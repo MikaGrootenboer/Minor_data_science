de2e63dac36bcc43102e7caff2d7a97f48badd2b
MikaGrootenboer <0925620@hr.nl>
2019-10-10 18:17:25 +0200

    updated some parts of the app

--- Project_plane/plane_filters.R
+++ Project_plane/plane_filters.R
@@ -14,11 +14,21 @@ library(stringr)
 #var1 = plane_data %>% filter(str_detect(plane_data$Summary,"Crash"))
 #This command will return all the objects where "Crash" is mentioned in the summary.
 
+#stepts to filter the dataset, we are not instereseted in the rows where there is no summary
+#These rows can be removed, to do that we can execute the following statements
+plane_withsum = plane_data
+plane_withsum = plane_withsum[rowSums(is.na(plane_withsum))==0,]
+plane_withsum = plane_withsum[plane_withsum$Summary!="",]
 #Next up are catagories, for the beginning I wil create different variables with hand of different causes.
-Planes_crashed = plane_data %>% filter(str_detect(plane_data$Summary,"Crash"))
-Planes_shot = plane_data %>% filter(str_detect(plane_data$Summary,"Shot"))
-Planes_lightning = plane_data %>% filter(str_detect(plane_data$Summary,"Struck by lightning"))
+Planes_crashed = plane_withsum %>% filter(str_detect(plane_withsum$Summary,c("Crash")))
+Planes_shot = plane_withsum %>% filter(str_detect(plane_withsum$Summary,c("Shot","shot")))
+Planes_lightning = plane_withsum %>% filter(str_detect(plane_withsum$Summary,"lightning"))
 Planes_caughtfire = plane_data %>% filter(str_detect(plane_data$Summary,"Caught fire"))
-Planes_exploded = plane_data %>% filter(str_detect(plane_data$Summary,"Exploded"))
+Planes_exploded = plane_data %>% filter(str_detect(plane_data$Summary,c("Exploded","explode")))
+Planes_hij = plane_data %>% filter(str_detect(plane_data$Summary,c("hijacked")))
 #possible way to filter it:
 #firecounter = nrow(plane_data %>% filter(str_detect(plane_data$Summary,"Caught fire")))
+
+
+#ignore the capital letters
+planes_crashed2 = plane_withsum %>% filter(str_detect(plane_withsum$Summary,fixed("crash", ignore_case=TRUE)))
