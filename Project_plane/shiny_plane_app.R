4296e936d725098df2f8579709d3d7bde9dca10b
Mika Grootenboer <0925620@hr.nl>
2019-10-16 14:07:06 +0200

    Update shiny_plane_app.R

--- Project_plane/shiny_plane_app.R
+++ Project_plane/shiny_plane_app.R
@@ -1,4 +1,13 @@
+#libs needed for the app to run
 library(shiny)
+library(dplyr)
+library(stringr)
+#import the plane crash data from the csv file.
+plane_data = read.csv("Airplane_Crashes2.csv")
+# remove the empty summary field and put it in a new variable.
+plane_withsum = plane_data
+plane_withsum = plane_withsum[rowSums(is.na(plane_withsum))==0,]
+plane_withsum = plane_withsum[plane_withsum$Summary!="",]
 
 ui = fluidPage(
   titlePanel("Data visualisation plane crashes"),
@@ -10,7 +19,7 @@ ui = fluidPage(
       checkboxGroupInput("cause", label = "choose a cause to filter on",
                          choices = list("Crashed" = "Crashed","Shot" = "Shot" ,"Struck by lightning" = "Struck by lightning",
                                         "Caught fire" = "Caught fire", "Exploded" = "Exploded"),
-                         selected = "Crashed"),
+                         selected = c("Crashed","Shot","Struck by lightning","Caught fire", "Exploded")),
       
       #This is the date range where the user will be able to filter the data by time.
       dateRangeInput(inputId = "Date_range", label = "Date range",start = "1908-01-01",end = "2012-01-01"),
@@ -21,6 +30,7 @@ ui = fluidPage(
     mainPanel(
       textOutput("test"),
       plotOutput("pie"),
+      plotOutput("bar"),
       tableOutput("table")
       
       
@@ -29,6 +39,7 @@ ui = fluidPage(
 )
 
 server = function(input,output){
+
   #piechart of the different ways a plane has crashed:
   output$pie = renderPlot(pie(c(nrow(plane_withsum %>% filter(str_detect(plane_withsum$Summary,fixed(input$cause[1], ignore_case=TRUE)))),
                                 nrow(plane_withsum %>% filter(str_detect(plane_withsum$Summary,fixed(input$cause[2], ignore_case=TRUE)))),
@@ -36,9 +47,18 @@ server = function(input,output){
                                 nrow(plane_withsum %>% filter(str_detect(plane_withsum$Summary,fixed(input$cause[4], ignore_case=TRUE)))),
                                 nrow(plane_withsum %>% filter(str_detect(plane_withsum$Summary,fixed(input$cause[5], ignore_case=TRUE))))),
                               c(input$cause), col = rainbow(8), radius = 0.9))
+
+
+  
+  output$bar = renderPlot(barplot(c(nrow(plane_withsum %>% filter(str_detect(plane_withsum$Summary,fixed(input$cause[1], ignore_case=TRUE)))),
+                                    nrow(plane_withsum %>% filter(str_detect(plane_withsum$Summary,fixed(input$cause[2], ignore_case=TRUE)))),
+                                    nrow(plane_withsum %>% filter(str_detect(plane_withsum$Summary,fixed(input$cause[3], ignore_case=TRUE)))),
+                                    nrow(plane_withsum %>% filter(str_detect(plane_withsum$Summary,fixed(input$cause[4], ignore_case=TRUE)))),
+                                    nrow(plane_withsum %>% filter(str_detect(plane_withsum$Summary,fixed(input$cause[5], ignore_case=TRUE))))
+                                    ),names.arg = c(input$cause[1],input$cause[2],input$cause[3],input$cause[4],input$cause[5]),col = rainbow(8)))
   
   #table for the raw results of the input field:
-  output$table = renderTable(spacing = c("s"),width = 200, plane_withsum[c(1,3,4,6,7,8,10,11,13)] %>% filter(str_detect(plane_withsum$Summary, fixed(input$cause, ignore_case=TRUE))))
+  output$table = renderTable(spacing = c("s"),width = 200, plane_withsum[c(1,3,4,6,7,10,13,17)] %>% filter(str_detect(plane_withsum$Summary, fixed(input$cause, ignore_case=TRUE))))
   
 
 }
